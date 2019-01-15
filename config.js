e0108da732c234e435d0ebe852345a9762365a6b
DESKTOP-425A87L\Erniukas <erniukas86@gmail.com>
2019-01-15 21:57:56 +0200

    update config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 1234,
+    PORT: 80,
     CONNECTION_STRING: 'mongodb://admin:admin123@ds157864.mlab.com:57864/academy',
     CONNECTION_STRING_TEST: 'mongodb://localhost:27017/test',
     TOKEN_TEST: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVjMDNiZGM2NTYzYjBhMGFmMGQyNTdiNyIsImVtYWlsIjoiYWRtaW4ifSwiaWF0IjoxNTQzNzkzMjI5fQ.kdtkUzpxmIrjOkglgxUVlE2gZf-em6vh7tqn83WauTk',
