7b3729dd5d20f59c71fcb79a9e32298b79f71b46
DESKTOP-425A87L\Erniukas <erniukas86@gmail.com>
2019-03-08 00:41:33 +0200

    Get group data implementation

--- controllers/group.js
+++ controllers/group.js
@@ -1,4 +1,5 @@
 const Group = require('../models/group');
+const Tournament = require('../models/tournament');
 const boom = require('boom');
 
 exports.create = async function(request, response, next){
@@ -16,9 +17,33 @@ exports.create = async function(request, response, next){
     }
 }
 
-exports.get = async function (request, response){
-    let groups = await Group.find().populate('participants');
-    response.send(groups);
+exports.get = async function (request, response, next){
+    try {
+        const { tournamentid } = request.query;
+        let groups = await Group.find({tournament: tournamentid})
+        .sort('name')
+        .populate('participants');
+
+        const tournament = await Tournament.findById(tournamentid);
+        let formedGroups = [];
+
+        if (tournament) {
+            for(let i = 0; i < groups.length; i++){
+                const group = groups[i];
+                let formedGroup = {
+                    name: group.name,
+                    _id: group._id,
+                    participants: []
+                };
+                formedGroup.participants = groupFormation[tournament.type](group);
+                formedGroups.push(formedGroup);
+            }
+        }
+
+        response.send(formedGroups);
+    } catch(error) {
+        next(boom.badData(error));
+    }
 }
 
 exports.addparticipants = async function (request, response, next){
@@ -29,4 +54,29 @@ exports.addparticipants = async function (request, response, next){
     }catch(error) {
         next(boom.badData(error));
     }
+}
+
+const groupFormation = {
+    Football: groupFootbalGroup
+}
+
+function groupFootbalGroup(group){
+    let formedGroup = [];
+    for(var i = 0; i < group.participants.length; i++){
+        let participant = group.participants[i];
+        let item = {
+            _id: participant._id,
+            name: participant.name,
+            GP: 0,
+            W: 0,
+            D: 0,
+            L: 0,
+            GF: 0,
+            GA: 0,
+            GD: 0,
+            PTS: 0
+        };
+        formedGroup.push(item);
+    }
+    return formedGroup;
 }
