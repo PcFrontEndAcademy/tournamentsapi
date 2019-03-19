bf05532e547037c7cb71ef04103e751563858a18
Ernestas Burokas <erniukas86@gmail.com>
2019-03-20 00:30:49 +0200

    Return 401 when user fails to login

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,18 @@
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const CONFIG = require('../config');
+const boom = require('boom');
 
-exports.login = async function(request, response){
+exports.login = async function(request, response, next){
     passport.authenticate('login', async (error, user, info) => {
         try{
             if(error || !user){
-                response.send(info);
+                next(boom.unauthorized());
             }
 
             request.login(user, {session: false}, async (error) => {
                 if(error){
-                    response.send(error);
+                    next(boom.unauthorized());
                 }
                 const body = {_id: user._id, email: user.email};
                 const token = jwt.sign({user: body}, CONFIG.JWT_SECRET);
@@ -19,7 +20,7 @@ exports.login = async function(request, response){
             });
 
         } catch(err){
-            response.send(err.message);
+            next(boom.unauthorized());
         }
 
     })(request, response);
